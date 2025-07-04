3f9f250a2c770edc12fcc19c9722a222625c8895
KingMaster86 <kingmastermassage2@gmail.com>
2025-07-04 20:10:14 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,30 @@ const urlsToCache = [
   "/icon-192.png",
   "/icon-512.png",
   "https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js",
-  "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js",
+  "const CACHE_NAME = "taxi-pi-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "style.css",
+  "manifest.json",
+  "firebase.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js",
   "https://www.gstatic.com/firebasejs/9.22.2/firebase-database-compat.js",
   // Google Maps loads dynamically, so can't cache directly
 ];
